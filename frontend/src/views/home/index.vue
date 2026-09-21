7052a9d18316cfc96acad9a86de7ac842ce05ae8
Alkaid <alkaid.ops@gmail.com>
2026-09-22 01:56:36 +0800

    fix: prevent mouse focus on floating buttons

--- frontend/src/views/home/index.vue
+++ frontend/src/views/home/index.vue
@@ -145,6 +145,10 @@ function scrollToTop() {
   scrollContainerRef.value?.scrollTo({ top: 0, behavior: 'smooth' })
 }
 
+function handleFloatingButtonMouseDown(event: MouseEvent) {
+  if (event.button === 0) event.preventDefault()
+}
+
 async function handleFloatingButtonClick(event: MouseEvent, action: () => unknown | Promise<unknown>) {
   const button = event.currentTarget as HTMLElement | null
   try {
@@ -960,12 +964,12 @@ function handleAddItem(itemIconGroupId?: number) {
     <!-- 悬浮按钮 -->
     <div v-if="parsePublicCodeFromPath() === '' && authStore.token" class="fixed-element shadow-[0_0_10px_2px_rgba(0,0,0,0.2)]">
       <NButtonGroup vertical>
-        <NButton data-testid="floating-refresh-button" color="#2a2a2a6b" :title="$t('common.refresh')" @click="handleFloatingButtonClick($event, refreshCurrentSpace)">
+        <NButton data-testid="floating-refresh-button" color="#2a2a2a6b" :title="$t('common.refresh')" @mousedown="handleFloatingButtonMouseDown" @click="handleFloatingButtonClick($event, refreshCurrentSpace)">
           <template #icon>
             <SvgIcon class="text-white font-xl" icon="material-symbols:refresh-rounded" />
           </template>
         </NButton>
-        <NButton data-testid="floating-top-button" color="#2a2a2a6b" :title="$t('spaceManage.backToTop')" @click="handleFloatingButtonClick($event, scrollToTop)">
+        <NButton data-testid="floating-top-button" color="#2a2a2a6b" :title="$t('spaceManage.backToTop')" @mousedown="handleFloatingButtonMouseDown" @click="handleFloatingButtonClick($event, scrollToTop)">
           <template #icon>
             <SvgIcon class="text-white font-xl" icon="icon-park-outline:to-top" />
           </template>
@@ -973,7 +977,7 @@ function handleAddItem(itemIconGroupId?: number) {
         <!-- 网络模式切换按钮组 -->
         <NButton
           v-if="panelState.networkMode === PanelStateNetworkModeEnum.lan && panelState.panelConfig.netModeChangeButtonShow" color="#2a2a2a6b"
-          data-testid="floating-wan-button" :title="t('panelHome.changeToWanModel')" @click="handleFloatingButtonClick($event, () => handleChangeNetwork(PanelStateNetworkModeEnum.wan))"
+          data-testid="floating-wan-button" :title="t('panelHome.changeToWanModel')" @mousedown="handleFloatingButtonMouseDown" @click="handleFloatingButtonClick($event, () => handleChangeNetwork(PanelStateNetworkModeEnum.wan))"
         >
           <template #icon>
             <SvgIcon class="text-white font-xl" icon="material-symbols:lan-outline-rounded" />
@@ -982,14 +986,14 @@ function handleAddItem(itemIconGroupId?: number) {
 
         <NButton
           v-if="panelState.networkMode === PanelStateNetworkModeEnum.wan && panelState.panelConfig.netModeChangeButtonShow" color="#2a2a2a6b"
-          data-testid="floating-lan-button" :title="t('panelHome.changeToLanModel')" @click="handleFloatingButtonClick($event, () => handleChangeNetwork(PanelStateNetworkModeEnum.lan))"
+          data-testid="floating-lan-button" :title="t('panelHome.changeToLanModel')" @mousedown="handleFloatingButtonMouseDown" @click="handleFloatingButtonClick($event, () => handleChangeNetwork(PanelStateNetworkModeEnum.lan))"
         >
           <template #icon>
             <SvgIcon class="text-white font-xl" icon="mdi:wan" />
           </template>
         </NButton>
 
-        <NButton data-testid="system-settings-button" color="#2a2a2a6b" @click="handleFloatingButtonClick($event, () => { settingModalShow = !settingModalShow })">
+        <NButton data-testid="system-settings-button" color="#2a2a2a6b" @mousedown="handleFloatingButtonMouseDown" @click="handleFloatingButtonClick($event, () => { settingModalShow = !settingModalShow })">
           <template #icon>
             <SvgIcon class="text-white font-xl" icon="majesticons-applications" />
           </template>
